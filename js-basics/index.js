9429f73cff04ad0f38211311f5d0c6e4a8c46021
Abijith <abijith220@hotmail.com>
2019-10-27 12:57:38 -0400

    added js-basics tutorial folder

---
+++ js-basics/index.js
@@ -0,0 +1,10 @@
+function greet(firstName,lastName){
+    console.log('wagwan my guy ' + firstName + ' ' + lastName)
+}
+
+greet('abi', 'mani')
+function square(number){
+    return number*number
+}
+
+console.log(square(2));
